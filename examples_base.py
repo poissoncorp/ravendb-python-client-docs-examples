57b2330d75af7f595fb636f0f98780d436a2c730
Gracjan Sadowicz <gracjan@ravendb.net>
2024-03-14 15:47:41 +0100

    Client API > Session > Querying > Sort query results (*)

--- examples_base.py
+++ examples_base.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from unittest import TestCase
 from ravendb import DocumentSession
-from ravendb.infrastructure.orders import Order, Address, OrderLine, Company, Employee as RavenEmployee
+from ravendb.infrastructure.orders import Order, Address, OrderLine, Company, Employee as RavenEmployee, Product
 from ravendb.tools.utils import Utils
 from ravendb_embedded import EmbeddedServer, ServerOptions
 
@@ -75,6 +75,82 @@ class ExampleBase(TestCase):
 
     @staticmethod
     def add_employees(session: DocumentSession):
-        session.store(Employee(first_name="John", last_name="Doe", birthday=datetime.today() - timedelta(days=10000)))
-        session.store(Employee(first_name="Jane", last_name="Doe", birthday=datetime.today() - timedelta(days=5000)))
+        session.store(
+            Employee(
+                first_name="John", last_name="Doe", birthday=datetime.today() - timedelta(days=10000), notes=["English"]
+            )
+        )
+        session.store(
+            Employee(
+                first_name="Jane", last_name="Doe", birthday=datetime.today() - timedelta(days=5000), notes=["Italian"]
+            )
+        )
+        session.save_changes()
+
+    @staticmethod
+    def add_products(session: DocumentSession):
+        session.store(
+            Product(
+                "products/1",
+                "Apple Vision Pro",
+                "suppliers/idream",
+                "categories/electronics",
+                "pcs",
+                5000,
+                12,
+                3,
+            )
+        )
+        session.store(
+            Product(
+                "products/2",
+                "Apple iCar",
+                "suppliers/2",
+                "categories/automotive",
+                "car",
+                99000,
+                57,
+                53,
+            )
+        )
+
+        session.store(
+            Product(
+                "products/4",
+                "Apple iJet",
+                "suppliers/3",
+                "categories/aircraft",
+                "plane",
+                14950000,
+                2,
+                1,
+            )
+        )
+
+        session.store(
+            Product(
+                "products/65",
+                "Apple iAirFry",
+                "suppliers/5",
+                "categories/kitchen",
+                "pcs",
+                14950,
+                52167,
+                1,
+                True,
+            )
+        )
+        session.store(
+            Product(
+                "products/65412",
+                "Apple iAntimatter",
+                "suppliers/x",
+                "categories/misc",
+                "fg",
+                600750399,
+                2,
+                0,
+                True,
+            )
+        )
         session.save_changes()
